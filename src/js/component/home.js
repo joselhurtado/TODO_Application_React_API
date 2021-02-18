1dbad2f53c58a714d6f2d43068e6be8e82d6ea24
Jose Hurtado <hello@josehurtado.net>
2021-02-18 01:38:40 +0000

    New CSS styles

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,49 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-//create your first component
+//TODOlist Function
 export function Home() {
-	const [task, setTask] = useState([
+	const [theList, getList] = useState([
 		"Learn Javascript",
 		"Schedule Mentorships",
 		"Practice React"
 	]);
-	const [userInput, setUserInput] = useState("");
+	const [userInput, setUserInput] = useState([""]); // First empty userInput
 
-	const handleKeyUp = () => {
-		if (event.keyCode === 13) {
-		} else {
+	const handleKeyUp = event => {
+		if (event.keyCode == 13 && userInput != "") {
+			getList(theList.concat(userInput));
+			setUserInput("");
 		}
 	};
+	// handleKeyUp from onKeyUp on input text with event passed as default
+	// check if event keycode is 13 (enter) and input is not blank to continue
+	// use state getList to add concat version of userInput into theList
+
+	const itemDelete = index => {
+		var updatedList = theList.filter(
+			(task, taskIndex) => index != taskIndex
+		);
+		getList(updatedList);
+	};
+	// create new variable with updated list > filter to check if index matches original index from list. then use getList to update to new list.
 
 	return (
-		<div className="text-center mt-5">
-			<div className="container form">
-				<h1 className="todo-title">TODO LIST</h1>
-				<div className="input-group mb-3">
-					<input
-						className="tasks form-control"
-						type="text"
-						onChange={event => setUserInput(event.target.value)}
-						value={userInput}
-						onKeyUp={handleKeyUp}
-						placeholder="Add a new task"
-						aria-label="Task on the list"
-						aria-describedby="basic-addon2"
-					/>
-					<span className="input-group-text" id="basic-addon2">
-						Add Item
-					</span>
-				</div>
-				<br />
+		<div className="container form text-center mt-5">
+			<h1>To-Do List</h1>
+			<br />
 
-				<div className="list-group">
-					{task.map((value, index) => {
-						return (
-							<li className="list-group-item" key={index}>
-								{value}
-							</li>
-						);
-					})}
+			<div className="container form-group todoList">
+				<input
+					className="taskInput"
+					onChange={event => setUserInput(event.target.value)}
+					value={userInput}
+					onKeyUp={handleKeyUp}
+					placeholder="Add a new task"
+					aria-label="Task on the list"
+					aria-describedby="basic-addon2"
+				/>
+				<br />
+				<div className="mt-3">
+					<ul className="taskGroup container-fluid">
+						{theList.map((value, index) => {
+							return (
+								<li className="list-group-item" key={index}>
+									{value}
+									<button
+										type="button"
+										onClick={() => itemDelete(index)}
+										className="close">
+										X
+									</button>
+								</li>
+							);
+						})}
+					</ul>
 				</div>
 			</div>
 		</div>
